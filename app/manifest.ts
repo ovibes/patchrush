2c67f062629a40380be6a4c08ca7e767f97dfc4d
ovibes <officevibes.loja@gmail.com>
2026-07-15 05:04:07 -0300

    Polish manifest app identity

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,6 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
+    id: "/",
     name: "PatchRush",
     short_name: "PatchRush",
     description:
