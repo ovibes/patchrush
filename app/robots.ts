c13231341becfe1189f836c2fd4f5aef6de71a83
ovibes <officevibes.loja@gmail.com>
2026-07-21 03:25:09 -0300

    Polish robots host metadata

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   const appUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
 
   return {
+    host: appUrl,
     rules: {
       userAgent: "*",
       allow: "/"
