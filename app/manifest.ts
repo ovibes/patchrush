06da4df792ecebb11b5ae14491c69982fc8fd59d
ovibes <officevibes.loja@gmail.com>
2026-07-25 13:55:56 -0300

    Relax PWA orientation lock

--- app/manifest.ts
+++ app/manifest.ts
@@ -10,7 +10,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     scope: "/",
     display: "standalone",
-    orientation: "portrait",
+    orientation: "any",
     background_color: "#090b10",
     theme_color: "#090b10",
     lang: "en",
