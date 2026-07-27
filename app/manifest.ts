4b22b6a153dd9526ea368914112dff43ced23a3d
ovibes <officevibes.loja@gmail.com>
2026-07-27 11:06:10 -0300

    Expose SVG icon in manifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -68,6 +68,12 @@ export default function manifest(): MetadataRoute.Manifest {
     ],
     icons: [
       {
+        src: "/icon.svg",
+        sizes: "any",
+        type: "image/svg+xml",
+        purpose: "any"
+      },
+      {
         src: "/icon-192.png",
         sizes: "192x192",
         type: "image/png",
